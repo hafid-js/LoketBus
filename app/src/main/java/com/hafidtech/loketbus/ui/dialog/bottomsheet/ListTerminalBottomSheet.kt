0427ffa6ef2a9ea634fa0745155d0179893c0c4b
hafidjs <afidzpratama@gmail.com>
2024-11-06 15:05:45 +0700

    Dialog BottomSheet Home Terminal Dari

--- app/src/main/java/com/hafidtech/loketbus/ui/dialog/bottomsheet/ListTerminalBottomSheet.kt
+++ app/src/main/java/com/hafidtech/loketbus/ui/dialog/bottomsheet/ListTerminalBottomSheet.kt
@@ -1,21 +1,66 @@
 package com.hafidtech.loketbus.ui.dialog.bottomsheet
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.bagicode.bagicodebaseutils.basewithbinding.BaseBindingBottomSheet
 import com.hafidtech.loketbus.R
+import com.hafidtech.loketbus.databinding.BottomSheetListTerminalBinding
+import com.hafidtech.loketbus.ui.dialog.bottomsheet.adapter.ListBottomTerminalAdapter
+import com.hafidtech.loketbus.ui.model.TerminalModel
+import okhttp3.internal.http2.Http2Connection.Listener
 
-class ListTerminalBottomSheet : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.bottom_sheet_list_terminal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class ListTerminalBottomSheet : BaseBindingBottomSheet(), ListBottomTerminalAdapter.ItemAdapterCallback {
+
+    private lateinit var binding : BottomSheetListTerminalBinding
+    private var dialogPosition : Int = 0
+    private var listener : Listener?= null
+    private lateinit var data : ArrayList<TerminalModel>
+    private lateinit var title : String
+    private lateinit var subtitle : String
+
+    override fun getFragmentView(): ViewBinding {
+        binding = BottomSheetListTerminalBinding.inflate(layoutInflater)
+        return binding
+    }
+
+    override fun onBindView() {
+        binding.tvTitle.text = title
+        binding.tvSubtitle.text = subtitle
+
+        var adapter = ListBottomTerminalAdapter(data, this)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
+        binding.rvListString.layoutManager = layoutManager
+        binding.rvListString.adapter = adapter
+        binding.rvListString.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+    }
+
+    override fun onListBottomTerminalClick(v: View, data: TerminalModel) {
+        dialog?.dismiss()
+        listener?.onClick(data)
+    }
+
+    interface Listener {
+        fun onClick(data : TerminalModel)
+    }
+
+    companion object {
+        fun newInstance(listener: Listener, position : Int, data : ArrayList<TerminalModel>,
+                        title : String, subtitle: String) : ListTerminalBottomSheet {
+            val instance = ListTerminalBottomSheet()
+            instance.listener = listener
+            instance.dialogPosition = position
+            instance.data = data
+            instance.title = title
+            instance.subtitle = subtitle
+            return instance
         }
     }
 }
--- app/src/main/java/com/hafidtech/loketbus/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/hafidtech/loketbus/ui/main/home/HomeFragment.kt
@@ -7,17 +7,39 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import com.bagicode.bagicodebaseutils.basewithbinding.BaseBindingFragment
 import com.hafidtech.loketbus.R
 import com.hafidtech.loketbus.databinding.FragmentHomeBinding
+import com.hafidtech.loketbus.ui.dialog.bottomsheet.ListTerminalBottomSheet
+import com.hafidtech.loketbus.ui.model.TerminalModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseBindingFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+    private lateinit var binding : FragmentHomeBinding
+    private var dataDari = ArrayList<TerminalModel>()
+
+    override fun getFragmentView(): ViewBinding {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding
+    }
+
+    override fun onBindView() {
+        initData()
+        binding.ivDari.setOnClickListener {
+
+            ListTerminalBottomSheet.newInstance(
+                object : ListTerminalBottomSheet.Listener {
+                    override fun onClick(data: TerminalModel) {
+                        binding.tvDariValue.text = data.namaTerminal
+                    }
+                },
+                0, dataDari, "Pilih keberangkatan", "Silahkan sesuaikan terminal keberangkatan Anda"
+            ).show(parentFragmentManager, "")
+        }
+    }
+    private fun initData() {
+        dataDari.add(TerminalModel("Terminal Pulo Gebang", "PGB"))
+        dataDari.add(TerminalModel("Terminal Kutoarjo", "KTJ"))
     }
 }
