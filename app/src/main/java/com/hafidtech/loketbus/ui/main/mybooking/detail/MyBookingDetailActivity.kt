9790cbc9b5ba41826a257c6fa5987dbf3955d86a
hafidjs <afidzpratama@gmail.com>
2024-11-14 21:17:08 +0700

    implementation data MyBooking Detail

--- app/src/main/java/com/hafidtech/loketbus/ui/main/mybooking/detail/MyBookingDetailActivity.kt
+++ app/src/main/java/com/hafidtech/loketbus/ui/main/mybooking/detail/MyBookingDetailActivity.kt
@@ -5,17 +5,54 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+import com.bagicode.bagicodebaseutils.basewithbinding.BaseBindingActivity
+import com.bagicode.bagicodebaseutils.utils.loadRoundedImage
 import com.hafidtech.loketbus.R
+import com.hafidtech.loketbus.databinding.ActivityMyBookingDetailBinding
+import com.hafidtech.loketbus.ui.model.response.MyBookingResponse
 
-class MyBookingDetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_my_booking_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class MyBookingDetailActivity : BaseBindingActivity() {
+
+    lateinit var binding : ActivityMyBookingDetailBinding
+
+    override fun getActivityBinding(): ViewBinding {
+        binding = ActivityMyBookingDetailBinding.inflate(layoutInflater)
+        return binding
+
+    }
+
+    override fun onBindView() {
+        intent.extras?.let {
+            var dataIntent = it.getParcelable<MyBookingResponse>("data")
+            initView(dataIntent)
         }
+    }
+
+    private fun initView(data: MyBookingResponse?) {
+        binding.tvCodeFrom.text = data?.codeTerminalDari
+        binding.tvCodeTo.text = data?.codeTerminalTujuan
+        binding.tvLocationFrom.text = data?.terminalDari
+        binding.tvLocationTo.text = data?.terminalTujuan
+
+        var showName=""
+        var showKursi=""
+
+        for (i in data?.penumpang!!.indices) {
+            showName = "${data?.penumpang?.get(i)?.nama}"
+            showKursi = "${data?.penumpang?.get(i)?.kursi}"
+        }
+
+        binding.tvName.text = showName.substring(0, showName.length-1)
+        binding.tvKursi.text = showKursi.substring(0, showKursi.length-1)
+
+        binding.tvDate.text = data?.dateKeberangkatan
+        binding.tvTime.text = data?.jamKeberangkatan
+
+        binding.ivLogo.loadRoundedImage(data?.busLogo, 4)
+        binding.tvTrans.text = data?.pembayaran
+
+        binding.tvPlat.text = data?.busPlat
+        binding.tvBooking.text = data?.idTiket
     }
 }
